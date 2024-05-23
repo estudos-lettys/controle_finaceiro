30d6bcad037f2a5cff689159f1efbc5cd4bd3c58
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2024-05-23 20:16:05 -0300

    feat: finalização dos eventos de click

--- src/scripts/renderValores.js
+++ src/scripts/renderValores.js
@@ -1,6 +1,7 @@
 //importações 
 import { insertedValues, valuesCategory } from "./valuesDatabase.js";
 
+//declarações
 const main = document.querySelector("main");
 const valores = document.querySelector("div.container_valores");
 const soma_total = document.querySelector("h3.soma_total");
@@ -9,33 +10,17 @@ const entrada = document.querySelector("button.entrada");
 const saida = document.querySelector("button.saida");
 
 //FUNÇÕES CASO ESTEJA SEM VALORES
-function renderizaSemValoresTodos() {
-    valores.innerHTML = "";
-    valores.innerHTML = `
-    <div class="valorNaoEncontrado">
-     <h3>Nenhum valor cadastrado</h3>
-    </div>
-   `;
-}
+function renderizaSemValores(title) {
 
-function renderizaSemValoresEntrada() {
     valores.innerHTML = `
     <div class="valorNaoEncontrado">
-     <h3> Sem valores na categoria Entradas</h3>
-    </div>
-   `;
-}
-
-function renderizaSemValoresSaida() {
-    valores.innerHTML = "";
-    valores.innerHTML = `
-    <div class="valorNaoEncontrado">
-     <h3> Sem valores na categoria Saídas</h3>
+     <h3>${title}</h3>
     </div>
    `;
 }
 
 //função para renderizar valores existentes 
+//TODOS
 function renderizaValoresExistentesTodos(data) {
     data.forEach((valor) => {
         const category = valor.categoryID == 0 ? "Entrada" : "Saída";
@@ -52,13 +37,60 @@ function renderizaValoresExistentesTodos(data) {
             `);
 
     });
+    renderSoma(data);
 }
+//ENTRADA
+function renderizaValoresExistentesEntradas(data) {
+    const filter = data.filter((valor) => {
+        return valor.categoryID == 0;
+    });
+
+    filter.forEach((valor) => {
+        const category = valor.categoryID == 0 ? "Entrada" : "Saída";
+        valores.insertAdjacentHTML("afterbegin",
+            `
+                <div class="valorEncontrado">
+                        <h3 class="valor_value">R$ ${valor.value.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</h3>
+                        <div class="category_button">
+                            <h3 class="valor_category">${category}</h3>
+                            <button class="valor_button"></button>
+                        </div>
+                </div>
+            `);
+    })
+
+    renderSoma(filter);
+}
+
+//SAIDA
+function renderizaValoresExistentesSaidas(data) {
+    const filter = data.filter((valor) => {
+        return valor.categoryID == 1;
+    });
+
+    filter.forEach((valor) => {
+        const category = valor.categoryID == 0 ? "Entrada" : "Saída";
+        valores.insertAdjacentHTML("afterbegin",
+            `
+                  <div class="valorEncontrado">
+                          <h3 class="valor_value">R$ ${valor.value.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</h3>
+                          <div class="category_button">
+                              <h3 class="valor_category">${category}</h3>
+                              <button class="valor_button"></button>
+                          </div>
+                  </div>
+              `);
+    })
+
+    renderSoma(filter);
+}
+
 //função para renderizar todos E TELA INICIAL (clamadas de função)
 
 function renderValoresTodos(data) {
     valores.innerHTML = "";
     if (data.length == 0) {
-        renderizaSemValoresTodos();
+        renderizaSemValores("Sem valores cadastrados");
     } else {
         renderizaValoresExistentesTodos(data);
     }
@@ -71,18 +103,32 @@ todos.addEventListener("click", () => {
     return renderValoresTodos(insertedValues);
 });
 
+//AÇÃO DE CLICK PARA ENTRADAS
+entrada.addEventListener("click", () => {
+    valores.innerHTML = "";
+    if (insertedValues.length == 0) {
+        renderizaSemValores("Sem valores na categoria Entradas");
+    } else {
+        renderizaValoresExistentesEntradas(insertedValues);
+    }
+});
 
+//AÇÃO DE CLICK PARA SAIDA
+saida.addEventListener("click", () => {
+    valores.innerHTML = "";
+    if (insertedValues.length == 0) {
+        renderizaSemValores("Sem valores na categoria Saídas");
+    } else {
+        renderizaValoresExistentesSaidas(insertedValues);
+    }
+});
 
 //FUNÇÃO PARA RENDERIZAR SOMA DOS VALORES 
 
 function renderSoma(data) {
     let reduz_soma = 0
     data.forEach((valor) => { reduz_soma += valor.value; });
-    soma_total.innerHTML += ` ${reduz_soma.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`
+    soma_total.innerHTML = "";
+    soma_total.innerHTML += `R$ ${reduz_soma.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`
 
 }
-
-renderSoma(insertedValues);
-
-
-//FUNÇÃO PARA RENDERIZAR FILTROS ENTRADA E SAIDA 
