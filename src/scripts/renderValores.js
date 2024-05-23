89b919e0e706f21b244f04b651d929d0e3dbdf73
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2024-05-22 22:05:23 -0300

    refactor: reorganização de arquivos

--- src/scripts/renderValores.js
+++ src/scripts/renderValores.js
@@ -1,9 +1,40 @@
 //importações 
-
 import { insertedValues, valuesCategory } from "./valuesDatabase.js";
-import { renderizaSemValoresTodos } from "./renderSemValores.js"
+
 const main = document.querySelector("main");
 const valores = document.querySelector("div.container_valores");
+const soma_total = document.querySelector("h3.soma_total");
+const todos = document.querySelector("button.todos");
+const entrada = document.querySelector("button.entrada");
+const saida = document.querySelector("button.saida");
+
+//FUNÇÕES CASO ESTEJA SEM VALORES
+function renderizaSemValoresTodos() {
+    valores.innerHTML = "";
+    valores.innerHTML = `
+    <div class="valorNaoEncontrado">
+     <h3>Nenhum valor cadastrado</h3>
+    </div>
+   `;
+
+}
+function renderizaSemValoresEntrada() {
+    valores.innerHTML = "";
+    valores.innerHTML = `
+    <div class="valorNaoEncontrado">
+     <h3> Sem valores na categoria Entradas</h3>
+    </div>
+   `;
+}
+
+function renderizaSemValoresSaida() {
+    valores.innerHTML = "";
+    valores.innerHTML = `
+    <div class="valorNaoEncontrado">
+     <h3> Sem valores na categoria Saídas</h3>
+    </div>
+   `;
+}
 
 //função para renderizar valores existentes 
 function renderValor(data) {
@@ -14,11 +45,11 @@ function renderValor(data) {
         } else {
             valor.categoryID = "Saída"
         }
-        
+
         valores.insertAdjacentHTML("afterbegin",
             `
        <div class="valorEncontrado">
-            <h3 class="valor_value">R$ ${valor.value.toLocaleString('pt-br', {minimumFractionDigits: 2})}</h3>
+            <h3 class="valor_value">R$ ${valor.value.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</h3>
             <div class="category_button">
                 <h3 class="valor_category">${valor.categoryID}</h3>
                 <button class="valor_button"></button>
@@ -30,7 +61,7 @@ function renderValor(data) {
     return render_valor;
 }
 
-//função para renderizar todos (clamadas de função)
+//função para renderizar todos E TELA INICIAL (clamadas de função)
 
 function renderValoresTodos(data) {
     if (data.length == 0) {
@@ -40,5 +71,25 @@ function renderValoresTodos(data) {
     }
 
 }
-
 renderValoresTodos(insertedValues);
+
+//AÇÃO DE CLICK PARA TODOS
+todos.addEventListener("click", () => {
+    return renderValoresTodos(insertedValues);
+});
+
+
+
+//FUNÇÃO PARA RENDERIZAR SOMA DOS VALORES 
+
+function renderSoma(data) {
+    let reduz_soma = 0
+    data.forEach((valor) => { reduz_soma += valor.value; });
+    soma_total.innerHTML += ` ${reduz_soma.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`
+
+}
+
+renderSoma(insertedValues);
+
+
+//FUNÇÃO PARA RENDERIZAR FILTROS ENTRADA E SAIDA 
